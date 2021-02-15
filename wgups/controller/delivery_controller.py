9e1cba707f26652139d29f90b366ba7c8ea55c22
Brian Oliver <brianolivergithub@gmail.com>
2021-02-15 10:35:27 -0500

    Documented clock.py and delivery_manager.py. Hard-coded 10:20 AM special event.

--- wgups/controller/delivery_controller.py
+++ wgups/controller/delivery_controller.py
@@ -5,7 +5,7 @@ from wgups.ds.graph import Graph
 from wgups.objects.clock import Clock
 from wgups.utils.data_reader import read_hubs, read_packages, create_trucks, create_vertexes, connect_vertexes, \
     load_truck
-import time
+import wgups.objects.package as package
 
 
 @dataclass()
@@ -18,6 +18,34 @@ class DeliveryController:
     }
 
     def start(self):
+        """Loads the data needed for the program to function.
+
+        The method does the following:
+            1. Creates a Graph instance to handle our vertices
+            representing each Hub object.
+
+            2. Calls wgups.utils.data_reader.read_hubs() to create a
+            Hub instance for each hub in the WGUPS Distance table.
+
+            3. Calls wgups.utils.data_reader.read_packages() to create a
+            package instance for each package in the WGUPS Package File.
+
+            4. Calls wgups.utils.data_reader.create_trucks() to create
+            3 Truck instances
+
+            5. Calls wgups.utils.data_reader.create_vertices() to
+            create a vertex instance to represent each Hub and
+            associates them with the Graph created in step 1.
+
+            6. Calls wgups.utils.data_reader.connect_vertices() to
+            create weighted undirected edges between all vertices.
+
+        Space Complexity
+            O(1)
+
+        Time Complexity
+            O(1)
+        """
         routes = Graph()
         read_hubs()
         read_packages()
@@ -26,11 +54,47 @@ class DeliveryController:
         connect_vertexes(routes)
 
     def tick(self):
+        """
+        Calls each function required to update the application
+        logic/data.
+
+        :return: None
+
+        Space Complexity
+            O(1)
+
+        Time Complexity
+            O(1)
+        """
         self._fire_special_events()
         print(self.clock)
         self.clock.add_minutes(5)
 
     def _fire_special_events(self):
+        """
+        This method is used to simulate the special events outlined in
+        the program requirements.
+
+        The following special events are hardcoded into the method:
+
+            1. At 8:00 AM, truck 1 is loaded and the truck status is
+            switched to active.
+
+            2. At 9:05 AM, The packages that were delayed are received.
+            Truck 2 and 3 are loaded, Truck 2 status is switched to
+            active.
+
+            3. At 10:20 AM, Package #9's address is updated to reflect
+            the corrected address
+
+        :return: None
+
+        Space Complexity:
+            O(1)
+
+        Time Complexity:
+            O(1)
+        """
         if self.clock.hour >= 8 and self.special_events.get("8:00 AM") is False:
             load_truck(1)
             truck.Truck.truck_list[0].toggle_status()
@@ -41,3 +105,9 @@ class DeliveryController:
             load_truck(2)
             load_truck(3)
             self.special_events["9:05 AM"] = True
+
+        if (self.clock.hour > 10 or (self.clock.hour == 10 and self.clock.minute >= 20)) and self.special_events.get("9:05 AM") is False:
+            cli.GUI.add_event("Package #9's address was corrected to 410 S State St.")
+            pkg = package.Package.package_list[8]
+            pkg.address = "410 S State St"
+            self.special_events["10:20 AM"] = True
--- wgups/objects/clock.py
+++ wgups/objects/clock.py
@@ -1,5 +1,23 @@
 class Clock:
+    """
+    Class used to represent and simulate time.
 
+    The class takes the total minutes elapsed and determines the
+    current time. For the purpose of this program, the class is
+    initialized with 480 total minutes so that it is 8:00 AM.
+
+    ...
+
+    Attributes
+        total_minutes: int Default: 480
+            The total number of minuted elapsed.
+        hour: int
+            The current hour in 24-hr format
+        minute: int
+            The current minute
+        meridian: str
+            The current "meridian" (AM or PM)
+    """
     def __init__(self, total_minutes: int = 480):
         self.total_minutes = total_minutes
         self.minute = total_minutes % 60
@@ -9,7 +27,20 @@ class Clock:
         else:
             self.meridian = "AM"
 
-    def add_minutes(self, minutes_to_add):
+    def add_minutes(self, minutes_to_add: int):
+        """Adds to the specified number of minutes to the Clock instance's
+        total_minutes and updates the instance's hour, minute, and
+        meridian properties.
+
+        :param minutes_to_add: int The number of minutes to increment
+        the clock instance's total_minutes by.
+
+        Space Complexity
+            O(1)
+
+        Time Complexity
+            O(1)
+        """
         self.total_minutes += minutes_to_add
         self.minute = self.total_minutes % 60
         self.hour = (self.total_minutes // 60) % 24
@@ -19,6 +50,21 @@ class Clock:
             self.meridian = "AM"
 
     def add_hour(self, hours_to_add):
+        """
+        Adds to the specified number of hours to the Clock instance's
+        total_minutes and updates the instance's hour and
+        meridian properties.
+
+        :param hours_to_add:
+            The number of hours to increment
+        the clock instance's hour by.
+
+        Space Complexity
+            O(1)
+
+        Time Complexity
+            O(1)
+        """
         self.total_minutes += hours_to_add
         self.minute = self.total_minutes % 60
         self.hour = (self.total_minutes // 60) % 24
@@ -28,17 +74,39 @@ class Clock:
             self.meridian = "AM"
 
     def __str__(self):
+        """
+        Displays the Clock instance as a "HH:MM AM/PM" string
+
+        :return: "HH:MM AM/PM"
+
+        Space Complexity
+            O(1)
+
+        Time Complexity
+            O(1)
+        """
         if self.hour >= 13:
-            strHr = self.hour % 12
+            str_hr = self.hour % 12
         else:
-            strHr = self.hour
+            str_hr = self.hour
 
         if self.minute < 10:
-            return f"{strHr}:0{self.minute} {self.meridian}"
+            return f"{str_hr}:0{self.minute} {self.meridian}"
         else:
-            return f"{strHr}:{self.minute} {self.meridian}"
+            return f"{str_hr}:{self.minute} {self.meridian}"
 
     def __repr__(self):
+        """
+        Displays the Clock instance as a "HH:MM AM/PM" string
+
+        :return: "HH:MM AM/PM"
+
+        Space Complexity
+            O(1)
+
+        Time Complexity
+            O(1)
+        """
         if self.hour >= 13:
             strHr = self.hour % 12
         else:
